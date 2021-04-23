68fe1457b639e7b99755a03f4d1bb8d0da5d935a
George Ho <george.h.w.ho@gmail.com>
2021-04-23 15:36:54 -0400

    Add styling for page

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,6 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Airline from './Airline'
+import styled from 'styled-components'
+
+const Home = styled.div`
+  text-align:center;
+`
+const Header = styled.div`
+  padding:100px 100px 10px 100px;
+
+  h1 {
+    font-size:42px;
+  }
+`
+
+const Subheader = styled.div`
+  font-weight:300;
+  font-size:26px;
+`
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  width: 100%;
+  padding:20px;
+`
 
 const Airlines = () => {
   const [airlines, setAirlines] = useState([])
@@ -27,17 +52,15 @@ const Airlines = () => {
   })
 
   return(
-    <div className="home">
-    <div className="header">
-      <h1>Airline Advisor</h1>
-      <p className="subheader">Honest, unbiased airline reviews. Share your experience.</p>
-    </div>
-    <div className="grid">
-      <ul>
+    <Home>
+      <Header>
+        <h1>Airline Advisor</h1>
+        <Subheader>Honest, unbiased airline reviews. Share your experience.</Subheader>
+      </Header>
+      <Grid>
         {grid}
-      </ul>
-    </div>
-  </div>
+      </Grid>
+    </Home>
   )
 }
 
