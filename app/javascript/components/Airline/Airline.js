b4b403d769fff48a4f4002e5386cf094b42418d7
George Ho <george.h.w.ho@gmail.com>
2021-04-23 17:51:39 -0400

    Add initial styling

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
+import Header from './Header'
 
-const Airline = () => {
+const Column = styled.div`
+  background: #fff;
+  max-width: 50%;
+  width: 50%;
+  float: left;
+  height: 100vh;
+  overflow-x: scroll;
+  overflow-y: scroll;
+  overflow: scroll;
+`
+
+const Airline = (props) => {
+  const [airline, setAirline] = useState({})
+  const [review, setReview] = useState([])
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect( () => {
+    const slug = props.match.params.slug
+    // Note: you can use string interpolation for this (`api/v1/${slug}`),
+    // my code highlighter is struggling with it so I'm doing this
+    const url = '/api/v1/airlines/' + slug
+
+    axios.get(url)
+    .then( (resp) => {
+      setAirline(resp.data)
+      setLoaded(true)
+      console.log(resp)
+    })
+    .catch( data => {
+      debugger
+    })
+  }, [])
+
   return(
-    <div>This is the Airline#show page for our app.</div>
-  )
-}
+    <div>
+      <Column>
+        {
+          loaded &&
+          <Header
+            attributes={airline.data.attributes}
+          />
+        }
 
-export default Airline
+        <div className="reviews">
+        </div>
+
+      </Column>
+      <Column>
+
+      </Column>
+    </div>
+    )
+  }
+
+  export default Airline
--- app/javascript/components/Airline/Header.js
+++ app/javascript/components/Airline/Header.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+
+
+const Wrapper = styled.div`
+  padding: 100px 100px 10px 100px;
+  font-size: 30px;
+  text-align: center;
+`
 
 const Header = (props) => {
   const {name, image_url} = props.attributes
 
   return(
-    <div className='header'>
+    <Wrapper>
       <img src={image_url} alt={name} width="50"/>
       <h1>{name}</h1>
-    </div>
+    </Wrapper>
   )
 }
 
